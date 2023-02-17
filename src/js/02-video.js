b77dab26ff0e634780b4bd1f9ca7dd71e4b0524d
VirLg <108211942+VirLg@users.noreply.github.com>
2023-02-17 14:23:14 +0200

    json

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from  '@vimeo/player'
+// window.Player = Player;
+// var player = new Player('vimeo-player')
 
+
+// function onYouTubeIframeAPIReady() {
+//    player = new YT.Player('vimeo-player', {
+//                          height: '500',
+// 			playerVars: { 'autoplay': 0, 'controls': 0, 'showinfo': 0, 'rel': 0},
+// 			width: '850',
+// 			videoId: 'CyVuYAHiZb8',
+// 			events: {
+// 			  'onReady': onPlayerReady
+// 			}
+// 		 });
+// }
+
+
+// 	function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('vimeo-player', {
+//         width: 600,
+//         height: 400,
+//         videoId: 'Xa0Q0J5tOP0',
+//         playerVars: {
+//             color: 'white',
+//             playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+//         },
+//         events: {
+//             onReady: initialize
+//         }
+//     });
+// }
