9ad3f52b0df40da9354085f3ed0db27627badcae
VirLg <108211942+VirLg@users.noreply.github.com>
2023-02-17 13:26:01 +0200

    gall

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const refUl = document.querySelector('.gallery')
+// refUl.addEventListener('click', onCloseModalWindow)
+
+function createItem({original, preview, description}) { 
+        return `
+    <a class="gallery__item"
+        href="${original}" onclick="return false">
+        <img class="gallery__image"
+        src="${preview}" 
+        alt="${description}" />
+    </a>         `      
+};
+
+
+const markupGallary= galleryItems.map(createItem).join('')
+
+refUl.insertAdjacentHTML("afterbegin", markupGallary)
+
+
+
+
+let gallery = new SimpleLightbox('.gallery a',
+    {
+        navText: ['←', '→'],
+        captionsData: 'alt',
+        animationSpeed: 1800,
+        animationSlide: true,
+        // swipeTolerance:50,
+        // fadeSpeed:1200,
+        captionDelay:250,
+    });
+
+
+// gallery.on('show.simplelightbox', function () {
+
+// 	// Do something…
+// });
+
+// gallery.on('error.simplelightbox', function (e) {
+// 	console.log(e); // Some usefull information
+// });
+
