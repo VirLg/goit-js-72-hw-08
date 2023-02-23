3073807d453b38ce8c69808bec9241cd80c410f5
VirLg <108211942+VirLg@users.noreply.github.com>
2023-02-23 22:03:24 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описаний в документації
 import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refUl = document.querySelector('.gallery')
