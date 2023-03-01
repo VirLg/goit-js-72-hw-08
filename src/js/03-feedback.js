0c24be8eee6b282ded01600f872f68023a1178fe
VirLg <108211942+VirLg@users.noreply.github.com>
2023-03-01 15:10:32 +0200

    a

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,55 +2,82 @@ const refForm = document.querySelector('.feedback-form');
 const refTextArea = document.querySelector('textarea');
 const refInput = document.querySelector("input")
 const submitBTN = document.querySelector('button')
-let objectValueForm = {}
-
+let objectValueForm = null;
+let responce = null;
+initForm()
 
 refForm.addEventListener('submit', onFormSubmit);
-
-
-initForm()
 
 function onFormSubmit(evt) {
     evt.preventDefault()
 
-    const validTextAteaVAlue = Boolean(refTextArea.value);
-    const validInputValue = Boolean(refInput.value)
-    
-    if ((evt.currentTarget === evt.target) === validTextAteaVAlue && validInputValue) {
-
-        const formData = new FormData(refForm)
-
-        formData.forEach((message, email) => {
-           
-            objectValueForm[email] = message;
+    const formData = new FormData(refForm)
+    formData.forEach((message, email) => {
+        if (message !== "" && email!==undefined) {
+            objectValueForm[email] = message
             localStorage.setItem("feedback-form-state", JSON.stringify(objectValueForm))
-        });
-    } else {
-        alert("заповніть будь ласка всі поля форми")
-    }
+            responce = localStorage.getItem("feedback-form-state")
+            responce = JSON.parse(responce)
+            objectValueForm = { ...responce }
+        } else {
+            alert("заповніть будь ласка всі поля форми")
+            
+        }
+  
+    
+   
+    })
+    console.log(objectValueForm);
 }
-// 
-let resp = localStorage.getItem("feedback-form-state")
-       response = JSON.parse(resp)
-objectValueForm = {...response}          
-console.log(objectValueForm);
+
 
 
 
 function initForm() {
-    let valueForm = localStorage.getItem("feedback-form-state")
-   
-    if (!valueForm) { 
-        return console.log("localStorage empty");
-    }
-
-refTextArea.textContent = Object.values(objectValueForm)
-    valueForm = JSON.parse(valueForm)
+  let a = localStorage.getItem("feedback-form-state")
+    a = JSON.parse(a)
     
-    refTextArea.textContent = valueForm.message
-    refInput.value = valueForm.email
+      if (!a) { 
+        return ;
+    }
+console.log(a);
+console.log(responce);
+  
+
+
+  
+    
+refInput.value = objectValueForm.email??""
+    refTextArea.textContent = objectValueForm.message
 
 }
+
+
+
+// function onFormSubmit(evt) {
+//     
+
+//     const validTextAteaVAlue = Boolean(refTextArea.value);
+//     const validInputValue = Boolean(refInput.value)
+//     //  
+    
+//     if ((evt.currentTarget === evt.target)=== validTextAteaVAlue && validInputValue) {
+
+//                   
+//     } else {
+
+//         
+//     }
+
+   
+// }
+
+//
+
+
+
+
+
 
 
 
