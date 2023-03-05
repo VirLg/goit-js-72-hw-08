594cbc34cdc30e7d67278cb43a20d17b82bb05af
VirLg <108211942+VirLg@users.noreply.github.com>
2023-03-05 15:36:37 +0200

    a

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,5 @@
 import  throttle  from 'lodash.throttle'
 
-
-
 const refForm = document.querySelector('.feedback-form');
 const refTextArea = document.querySelector('textarea');
 const refInput = document.querySelector("input")
@@ -15,40 +13,52 @@ refForm.addEventListener('submit', onFormSubmit);
 function onFormSubmit(evt) {
     evt.preventDefault()
 
+    if (refInput.value !== "" && refTextArea.value !== "") {
+
+  
+    evt.currentTarget.reset()
+    localStorage.removeItem('feedback-form-state')
+    console.log(populateValue);
+    } else { 
+            alert("Заповніть поля форми")
+        }
+
+}
+refForm.addEventListener('input', throttle(handleValue, 500))
+
+refForm.addEventListener("input", handleValue)
+
+
+function handleValue(evt) {
     const formData = new FormData(refForm)
     formData.forEach((message, email) => {
-        if (message !== "" && email!==undefined) {
+        
             objectValueForm[email] = message
             localStorage.setItem("feedback-form-state", JSON.stringify(objectValueForm))
             responce = localStorage.getItem("feedback-form-state") ?? {};
             responce = JSON.parse(responce)
             objectValueForm = { ...responce }
-        } else {
-            alert("заповніть будь ласка всі поля форми")
-            
-        }
        
-    })
-   console.log(objectValueForm);
- 
+     } )   
+       
 }
 
-  
-if (!refTextArea.value && !refInput.value) {
+let populateValue = localStorage.getItem('feedback-form-state')
+populateValue = JSON.parse(populateValue) ?? {}
 
-    let storage = localStorage.getItem("feedback-form-state")
-    storage = JSON.parse(storage) ?? {}
 
-    Object.entries(storage).forEach(([email, value],idx,arr) => {
-        refTextArea.value = arr[1][idx];
-        refInput.value = arr[0][idx];    
-    })  
-}
-   
+Object.entries(populateValue).forEach(([email, message], idx, arr) => {
+
+            refTextArea.value = populateValue.message??"";
+            refInput.value = populateValue.email??'';
+})
 
 
 
-refTextArea.addEventListener('input', throttle(() => { }, 500))
+
+
+
+
 
 
 
@@ -63,3 +73,130 @@ refTextArea.addEventListener('input', throttle(() => { }, 500))
 //     );
 //   }, 300),
 // );
+
+
+// import  throttle  from 'lodash.throttle'
+
+
+
+// const refForm = document.querySelector('.feedback-form');
+// const refTextArea = document.querySelector('textarea');
+// const refInput = document.querySelector("input")
+// const submitBTN = document.querySelector('button')
+// let objectValueForm = {} ;
+// let responce = {};
+
+
+// refForm.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(evt) {
+//     evt.preventDefault()
+
+//     const formData = new FormData(refForm)
+//     formData.forEach((message, email) => {
+//         if (message !== "" && email!==undefined) {
+//             objectValueForm[email] = message
+//             localStorage.setItem("feedback-form-state", JSON.stringify(objectValueForm))
+//             responce = localStorage.getItem("feedback-form-state") ?? {};
+//             responce = JSON.parse(responce)
+//             objectValueForm = { ...responce }
+//         } else {
+//             alert("заповніть будь ласка всі поля форми")
+            
+//         }
+       
+//     })
+//    console.log(objectValueForm);
+ 
+// }
+
+  
+// if (refTextArea.value && refInput.value) {
+
+//     let storage = localStorage.getItem("feedback-form-state")
+//     storage = JSON.parse(storage) ?? {}
+
+//     Object.entries(storage).forEach(([email, value],idx,arr) => {
+//         refTextArea.value = arr[1][idx];
+//         refInput.value = arr[0][idx];    
+//     })  
+// }
+   
+
+// refForm.addEventListener('input', handleInput)
+
+// function handleInput(evt) { 
+
+// localStorage.setItem("feedback-form-state", JSON.stringify(objectValueForm.email[evt.target.value]))
+
+// console.log(evt.target);
+// }
+
+
+
+// refTextArea.addEventListener('input', throttle(() => { }, 500))
+
+
+
+// ==========================================================
+
+
+// import  throttle  from 'lodash.throttle'
+
+
+
+// const refForm = document.querySelector('.feedback-form');
+// const refTextArea = document.querySelector('textarea');
+// const refInput = document.querySelector("input")
+// const submitBTN = document.querySelector('button')
+// let objectValueForm = {} ;
+// let responce = {};
+
+// populateTextArea()
+// refForm.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(evt) {
+//     evt.preventDefault()
+
+//     const formData = new FormData(refForm)
+//     formData.forEach((message, email) => {
+//         if (message !== "" && email!==undefined) {
+//             objectValueForm[email] = message
+//             localStorage.setItem("feedback-form-state", JSON.stringify(objectValueForm))
+//             responce = localStorage.getItem("feedback-form-state") ?? {};
+//             responce = JSON.parse(responce)
+//             objectValueForm = { ...responce }
+//             evt.currentTarget.reset()
+//             localStorage.removeItem('feedback-form-state')
+//             localStorage.removeItem('msg-textArea')
+//             localStorage.removeItem('msg-input')
+//         } else {
+//             alert("заповніть будь ласка всі поля форми")
+            
+//         }
+       
+//     })
+//    console.log(objectValueForm);
+ 
+// }
+// refTextArea.addEventListener('input', throttle(() => { }, 1500))
+
+// refForm.addEventListener("input", handleValue)
+
+// function handleValue(evt) { 
+//     const msgTextArea = refTextArea.value
+//     const msgInput = refInput.value
+//     localStorage.setItem('msg-textArea', msgTextArea)
+//     localStorage.setItem('msg-input', msgInput)
+// }
+
+// function populateTextArea() { 
+//     const popuateTextareaValue = localStorage.getItem('msg-textArea')
+//     const popuateInputValue = localStorage.getItem('msg-input')
+//     refTextArea.value = popuateTextareaValue ?? "";
+//     refInput.value = popuateInputValue ?? "";
+
+// }
+
+
+//   ======================================================
